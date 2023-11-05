dd1dfbbbc2ac0046f9e9a3fc8f3f5fe6b71dcb54
Chandirasegaran <chandirasegaransegar@gmail.com>
2023-11-05 22:36:59 +0530

    added phone and email validator

--- WT Ex 1/assets/Scripts/app.js
+++ WT Ex 1/assets/Scripts/app.js
@@ -10,3 +10,36 @@ const rfileChosen = document.getElementById('res-chosen');
 ractualBtn.addEventListener('change', function () {
   rfileChosen.textContent = this.files[0].name
 })
+
+
+// Phone Number Validation
+
+const phoneInput = document.getElementById('phone');
+const plable = document.getElementById('phonelabid');
+
+phoneInput.addEventListener('input', function (event) {
+  const sanitizedValue = phoneInput.value.replace(/\D/g, '');
+  phoneInput.value = sanitizedValue;
+  if (sanitizedValue.length === 10 || sanitizedValue.length === 0) {
+    phoneInput.style.borderColor = ''; 
+    phoneInput.setCustomValidity(''); 
+    plable.innerHTML= 'Phone Number';
+    plable.style.color='';
+  } else {
+    phoneInput.style.borderColor = 'red'; 
+    phoneInput.setCustomValidity(''); 
+    plable.innerHTML= 'Enter valid Phone Number';
+    plable.style.color='red';
+  }
+});
+
+// Email Validation
+
+const emailInput = document.getElementById('emailinput');
+const elabel = document.getElementById('emaillabel');
+
+emailInput.addEventListener('input', function(event)){
+  const emailvalue=emailInput.value;
+  const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.(edu|com|net|org|in)$/;
+  
+}
